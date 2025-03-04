264125a3ba7e0800bc47cced375fd39355e43a9b
Michelle <bonillamich55@gmail.com>
2025-03-04 00:15:29 -0500

    script updated

--- redondear.js
+++ redondear.js
@@ -1,9 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const boton = document.getElementById("boton");
-    const cajita1 = document.getElementById("box1");
+  const boton = document.getElementById("boton");
+  let cajita1 = document.getElementById("box1");
 
-    boton.addEventListener("click", function () {
-      cajita1.classList.toggle("js-class");
-      cajita1.innerText = "Hola mamor";
-    });
-})
+  boton.addEventListener("click", function () {
+    boton.innerText = "Auch!";
+    cajita1.classList.add("js-class");
+    cajita1.classList.remove("js-class");
+
+    cajita1.classList.toggle("js-class");
+
+    cajita1.innerHTML = "<div><p> soy otro div </p></div>";
+  });
+});
